75a652e7b191cba8f160e234d18d5382a619076e
Valentina Buyalskaya <vakabubu@yandex.ru>
2025-10-06 16:46:12 +0300

    Import CSV, added extra info for README

--- api_yamdb/reviews/management/commands/load_comments.py
+++ api_yamdb/reviews/management/commands/load_comments.py
@@ -2,8 +2,10 @@ import csv
 
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
+from django.db.utils import IntegrityError
+from django.shortcuts import get_object_or_404
 
-from reviews.models import Comment
+from reviews.models import Comment, Review
 
 
 User = get_user_model()
@@ -13,20 +15,20 @@ class Command(BaseCommand):
     help = "Импортируем комментарии."
 
     def handle(self, *args, **options):
-        comments_to_create = []
         with open('static/data/comments.csv', mode='r',
                   encoding='utf-8') as file:
             reader = csv.DictReader(file)
             for row in reader:
-                comments_to_create.append(
-                    Comment(
+                review = get_object_or_404(Review, id=row['review_id'])
+                author = get_object_or_404(User, id=row['author'])
+                try:
+                    Comment.objects.create(
                         id=row['id'],
-                        review_id=row['review_id'],
-                        author_id=row['user_id'],
+                        review=review,
+                        author=author,
                         text=row['text'],
                         pub_date=row['pub_date']
                     )
-                )
-        Comment.objects.bulk_create(comments_to_create)
-        self.stdout.write(
-            f'Импортировано {len(comments_to_create)} комментариев.')
+                except IntegrityError as e:
+                    self.stdout.write(f'Не удалось добавить комментарии: {e}')
+
