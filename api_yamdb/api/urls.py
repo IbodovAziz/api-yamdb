efbbea96a2ffb35e8ee37c726f44903427384b78
Aziz <ibodov.aziz@yandex.ru>
2025-10-11 22:16:11 +0300

    Добавил 2 роутера

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedSimpleRouter
 
 from .views import (
     AuthViewSet,
@@ -18,30 +19,14 @@ router_v1.register('genres', GenreViewSet, basename='genres')
 router_v1.register('titles', TitleViewSet, basename='titles')
 router_v1.register('users', UserViewSet, basename='users')
 
-review_list = ReviewViewSet.as_view({'get': 'list', 'post': 'create'})
-review_detail = ReviewViewSet.as_view({
-    'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'
-})
+titles_router = NestedSimpleRouter(router_v1, r'titles', lookup='title')
+titles_router.register(r'reviews', ReviewViewSet, basename='title-reviews')
 
-comment_list = CommentViewSet.as_view({'get': 'list', 'post': 'create'})
-comment_detail = CommentViewSet.as_view({
-    'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy'
-})
-
-nested_urls = [
-    path('titles/<int:title_id>/reviews/', review_list, name='review-list'),
-    path('titles/<int:title_id>/reviews/<int:pk>/',
-         review_detail, name='review-detail'),
-    path('titles/<int:title_id>/reviews/<int:review_id>/comments/',
-         comment_list, name='comment-list'),
-    path(
-        'titles/<int:title_id>/reviews/<int:review_id>/comments/<int:pk>/',
-        comment_detail,
-        name='comment-detail'
-    ),
-]
+reviews_router = NestedSimpleRouter(titles_router, r'reviews', lookup='review')
+reviews_router.register(r'comments', CommentViewSet, basename='review-comments')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
-    path('v1/', include(nested_urls))
+    path('v1/', include(titles_router.urls)),
+    path('v1/', include(reviews_router.urls))
 ]
